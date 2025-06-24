6634553ec6ecedf8b1fc0c7fa1eb6d08393ee6cf
Entropire <158569522+Entropire@users.noreply.github.com>
2025-06-24 13:53:12 +0200

    setup react-router-dom with basic routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { Routing } from './routing';
 import './css/index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <BrowserRouter>
+      <Routing/>
+    </BrowserRouter>
   </StrictMode>,
 )
---
+++ src/routing.tsx
@@ -0,0 +1,13 @@
+import { Routes, Route } from "react-router-dom"
+
+export const Routing = () => {
+
+    return (
+        <>
+        
+            <Routes>
+
+            </Routes>
+        </>
+    )
+}
