df6346322c54142b2688cdddf4030830ffab7a72
Entropire <158569522+Entropire@users.noreply.github.com>
2025-06-24 14:15:25 +0200

    added a temp main page for the live server

---
+++ src/pages/home.tsx
@@ -0,0 +1,12 @@
+import "../css/pages/home.css";
+
+export const Home = () => {
+    return(
+        <>
+            <h1>Welcome to my portfolio</h1>
+            <h2>Right now its under construction</h2>
+            <h2>But here is a cat for you to look at</h2>
+            <img src="/cat.jpg" alt="This should be a cat image" />
+        </>
+    );
+}
--- src/routing.tsx
+++ src/routing.tsx
@@ -1,12 +1,12 @@
 import { Routes, Route } from "react-router-dom"
+import { Home } from "./pages/home.tsx"
 
 export const Routing = () => {
 
     return (
         <>
-        
             <Routes>
-
+                <Route path={"/"} element={<Home/>} />
             </Routes>
         </>
     )
