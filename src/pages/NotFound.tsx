aa7a492f5c862ed869d574887bfc95a040772b7e
Entropire <158569522+Entropire@users.noreply.github.com>
2025-06-25 15:23:34 +0200

    Add custom 404 page with image and styling

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -3,6 +3,10 @@ import "../css/pages/NotFound.css";
 export const NotFound = () => {
     return(
         <>
+            <div className="Page-Not-Found">
+                <img src="./404.jpg" alt="Hiding cat" />
+                <h2>404 - Page not found</h2>
+            </div>
         </>
     );
 }
