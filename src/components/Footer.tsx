f46725b85088a959ee26830c1cc1caccec73f945
Entropire <158569522+Entropire@users.noreply.github.com>
2025-06-25 23:43:42 +0200

    Update footer links with correct URLs

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,9 +7,9 @@ export const Footer = () => {
                 <p className='Footer-Left'>© {new Date().getFullYear()} Quinten Duijsyer - All rights reserved</p>
                 <nav className='Footer-Right'>
                     <a href=''><img src="./icons/CV.png" alt="CV" /></a>
-                    <a href=''><img src="./icons/E-mail.png" alt="E-mail" /></a>
-                    <a href=''><img src="./icons/Linkedin.png" alt="Linkedin" /></a>
-                    <a href=''><img src="./icons/Github.png" alt="Github" /></a>
+                    <a href='mailto:qduyster14@gmail.com'><img src="./icons/E-mail.png" alt="E-mail" /></a>
+                    <a href='https://www.linkedin.com/in/quinten-duijster-0b60a9318' target='blank'><img src="./icons/Linkedin.png" alt="Linkedin" /></a>
+                    <a href='https://github.com/Entropire' target='blank'><img src="./icons/Github.png" alt="Github" /></a>
                 </nav>
             </footer>
         </>
