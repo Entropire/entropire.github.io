fd428f2303d093c2dc041b61ed3612940915734f
Entropire <158569522+Entropire@users.noreply.github.com>
2025-06-28 13:42:01 +0200

    BugFix: 404-image cant be found in heigher path segmants then 1.

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,10 +1,11 @@
 import "../css/pages/NotFound.css";
+import image from "../assets/404.jpg";
 
 export const NotFound = () => {
     return(
         <>
             <div className="Page-Not-Found">
-                <img src="./404.jpg" alt="Hiding cat" />
+                <img src={image} alt="Hiding cat" />
                 <h2>404 - Page not found</h2>
             </div>
         </>
