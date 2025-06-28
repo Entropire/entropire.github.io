358cd12630acab903d76ba5d3963cc6fbf99b65b
Entropire <158569522+Entropire@users.noreply.github.com>
2025-06-28 14:25:17 +0200

    Move icon assets and update Footer imports

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,8 @@
 import FooterCSS from '../css/components/Footer.module.css'
+import CV_Icon from '../assets/icons/CV.png'
+import Email_Icon from '../assets/icons/E-mail.png'
+import Linkedin_Icon from '../assets/icons/Linkedin.png'
+import Github_Icon from '../assets/icons/Github.png'
 
 export const Footer = () => {
     return(
@@ -6,10 +10,10 @@ export const Footer = () => {
             <footer className={FooterCSS.Footer}>
                 <p className={FooterCSS["Footer-Left"]}>© {new Date().getFullYear()} Quinten Duijster - All rights reserved</p>
                 <nav className={FooterCSS["Footer-Right"]}>
-                    <a href=''><img src="./icons/CV.png" alt="CV" /></a>
-                    <a href='mailto:qduyster14@gmail.com'><img src="./icons/E-mail.png" alt="E-mail" /></a>
-                    <a href='https://www.linkedin.com/in/quinten-duijster-0b60a9318' target='blank'><img src="./icons/Linkedin.png" alt="Linkedin" /></a>
-                    <a href='https://github.com/Entropire' target='blank'><img src="./icons/Github.png" alt="Github" /></a>
+                    <a href=''><img src={CV_Icon} alt="CV" /></a>
+                    <a href='mailto:qduyster14@gmail.com'><img src={Email_Icon} alt="E-mail" /></a>
+                    <a href='https://www.linkedin.com/in/quinten-duijster-0b60a9318' target='blank'><img src={Linkedin_Icon} alt="Linkedin" /></a>
+                    <a href='https://github.com/Entropire' target='blank'><img src={Github_Icon} alt="Github" /></a>
                 </nav>
             </footer>
         </>
