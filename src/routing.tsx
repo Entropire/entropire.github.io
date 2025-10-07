fd5cdd2dd863774d4ec395d339a29a3b9ccb7f64
Entropire <158569522+Entropire@users.noreply.github.com>
2025-10-07 12:25:52 +0200

    Remove duplicate Contact page and unused styles

--- src/routing.tsx
+++ src/routing.tsx
@@ -2,11 +2,11 @@ import { Routes, Route } from "react-router-dom"
 import { Home } from "./pages/home.tsx"
 import { Header } from "./components/Header.tsx"
 import { Footer } from "./components/Footer.tsx"
-import { About } from "./pages/about.tsx"
-import { Contact } from "./pages/contact.tsx"
-import { NotFound } from "./pages/notFound.tsx"
-import { Projects } from "./pages/projects.tsx"
-import { Project } from "./pages/project.tsx"
+import { About } from "./pages/About.tsx"
+import { Contact } from "./pages/Contact.tsx"
+import { NotFound } from "./pages/NotFound.tsx"
+import { Projects } from "./pages/Projects.tsx"
+import { Project } from "./pages/Project.tsx"
 import './styles/Global.css'
 
 export const Routing = () => {
